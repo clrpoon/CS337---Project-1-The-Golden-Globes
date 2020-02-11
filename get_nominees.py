dbcc58cd03daaa1cd0153490debccfaef6375be8
Jonathan Dai <jonathandai@Jonathans-MacBook-Pro.local>
2020-02-11 16:04:58 -0600

    added nominees and presenters code

--- get_nominees.py
+++ get_nominees.py
@@ -1,5 +1,91 @@
+import nltk
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+import numpy
+import re
+import copy
+from nltk.stem import PorterStemmer
 from util import *
 
+tokenizer = TweetTokenizer()
+stop_words = list(stopwords.words("english")) + ['performance','golden', 'globes', 'globe', 'goldenglobes', 'best', 'losers', 'loser', 'winners', 'winner', 'actor', 'actress', 'actors', 'actresses', 'yay', 'congrats']
+
+def find_nominees(tweet, set_nom, award):
+    # clean and tokenize
+    o_text = clean_tweet(tweet)
+    text = tokenizer.tokenize(o_text)
+    # remove stop words
+    clean_text = []
+    for t in text:
+        if t not in stop_words:
+            clean_text.append(t)
+    # tag tokenized tweet
+    tagged = nltk.pos_tag(clean_text)
+    names = []
+    portstemmer = PorterStemmer()
+    stemmed = []
+    for token in [c.lower() for c in clean_text]:
+        stemmed.append(portstemmer.stem(token))
+    nomin_found = False
+    for t in nltk.ne_chunk(tagged):
+        if type(t) == nltk.tree.Tree:
+            valid = True
+            if t.label() == 'PERSON':
+                poss = []
+                for c in t:
+                    if str(c[0]).lower() not in award.lower() and str(c[0]).lower() not in stop_words:
+                        poss.append(str(c[0]))
+                names.append(' '.join([p for p in poss]))
+    set_nom_copy = copy.deepcopy(set_nom)
+
+    # check for duplicates
+    for n in names:
+        if len(set_nom) > 0:
+            for s in set_nom_copy:
+                if s in n and s in set_nom:
+                    set_nom.remove(s)
+                    set_nom.add(n)
+                elif n not in s :
+                    set_nom.add(n)
+        else:
+            set_nom.add(n)
+    return set_nom
+
+def filter_tweets_by_award(tweet_list, award):
+    award_stripped = award
+    for word in stop_words:
+        award_stripped = award_stripped.replace(" " + word + " ", ' ')
+    award_no_punct = re.sub(r'[^\w\s]', '', award_stripped)
+
+    tweets = tweet_list
+    for token in tokenizer.tokenize(award_no_punct):
+        filtered = []
+        for text in tweets:
+            found =  re.search(token, text) 
+            if(found):
+                filtered.append(text)
+        if len(filtered) > 0:
+            tweets = filtered
+    return tweets
+
 def _get_nominees(tweet_list, award_list):
-    nominees = dict.fromkeys(award_list)
+    # remove retweets
+    rt_filter = []
+    for tweet in tweet_list:
+        found = re.search("RT", tweet, flags=re.IGNORECASE)
+        if(not found):
+            rt_filter.append(tweet)
+    tweet_list = rt_filter
+    nominees = {}
+    for award in award_list:
+        award_tweet_list = filter_tweets_by_award(tweet_list, award)
+        award_nominees = set()
+        for tweet in award_tweet_list:
+            nom_set = find_nominees(tweet, award_nominees, award)
+        for nom in nom_set:
+            award_nominees.add(nom)
+        nominees[award] = list(award_nominees)
     return nominees
--- get_presenters.py
+++ get_presenters.py
@@ -1,2 +1,62 @@
+def filter_presenter_tweets(data, award):
+    tweets_with_presenters = []
+    # non important award words        
+    award_stop_words = ["best", "by", "a", "an", "for", "in", "or", "-"]
+    # important words for this award 
+    award_keywords = []
+    for word in award.split():
+        if word not in award_stop_words:
+            award_keywords.append(word)
+#     print(award_keywords)
+    present_words = ["present", "presented", "presents", "presentor", "presentors"]
+    for tweet in gg_data: 
+        tweet_text = tweet['text']
+        if any(pres_keyword in tweet_text.lower() for pres_keyword in present_words):
+#             count = 0 
+#             for k in award_keywords:
+#                 if k in tweet_text.lower():
+#                     count+=1
+#             if count/len(award_keywords) >= 0.5:
+#             print(tweet_text)
+#             print("-------------------------------------------------------------------------------------")
+            tweets_with_presenters.append(tweet_text)
+    return tweets_with_presenters
+
 def _get_presenters(data, awards):
-    return {}
+    '''Hosts is a list of one or more strings. Do NOT change the name
+    of this function or what it returns.'''
+    # declare dictionary for winners (award: winner)
+    presenters = {key: "" for key in awards} 
+    
+    for award in awards:
+    # Get tweets with keyword award 
+        tweets_with_award = filter_presenter_tweets(data, award)
+#         print(tweets_with_award)
+#         tweets_with_award = preprocess_tweets(tweets_with_award)
+#         tweets_with_award = remove_stop_words_from_tweets(tweets_with_hosts, ['http', 'golden', 'gg', '@'])
+        
+        presenter_name_count = {}
+        for tweet in tweets_with_award: 
+            # movies are categorized as WORK_OF_ART
+            entity_type = "PERSON"
+            names = get_type_of_name(tweet, entity_type)
+            for name in names :
+                if name.lower() in ["#goldenglobes", "rt @goldenglobes", "cecil b. demille", "goldenglobes", "goldenglobe", "golden", "globes", "golden globes", "golden globe"]:
+                    continue
+                if name in presenter_name_count :
+                    presenter_name_count[name] = presenter_name_count[name] + 1
+                else:
+                    presenter_name_count[name] = 1
+#         print(award, nominee_name_count)
+        two_presenters = [] 
+        for i in range(2): 
+            try:
+                top_presenter = max(presenter_name_count.items(), key=operator.itemgetter(1))[0]
+                two_presenters.append(top_presenter)
+                del top_presenter[top_nominee]
+            except: 
+                two_presenters.append("")
+        presenters[award] = two_presenters
+        print(award,presenters[award])
+        print('-----------------------------------------')
+    return presenters 
